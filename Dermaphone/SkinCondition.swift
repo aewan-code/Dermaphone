3e2acfc6e35b4cd420dadc1304aef838d7c15e84
aewan-code <aleera.ewan@outlook.com>
2024-04-02 21:09:36 +0100

    Created skinCondition class and its viewcontroller

--- Dermaphone/SkinCondition.swift
+++ Dermaphone/SkinCondition.swift
@@ -11,18 +11,29 @@ class SkinCondition{
     let name : String
     let description : String?//are they required when creating the skin condition package?
     let cause : String?
-    //let image : ImageLoader
+    let image : UIImage?
     let texture : String?//features e.g. colour
     let diagnosis : String?
     let treatment: String?
-    let model : skinmodel//Should this be a view controller
-    init(name : String, description : String, cause : String, texture : String, diagnosis : String, treatment : String, model : skinmodel){
+    let model : [skinmodel]//Should this be a view controller? List of all models that are this skin condition
+    init(name : String, description : String, cause : String, texture : String, diagnosis : String, treatment : String, model : skinmodel, image : UIImage){
         self.name = name
         self.description = description
         self.cause = cause
         self.texture = texture
         self.diagnosis = diagnosis
         self.treatment = treatment
-        self.model = model
+        self.model = [model]
+        self.image = image
+    }
+    
+    //set image - either from gallery or from camera or from model (screenshot) (ideally needs to be top down view). Can it get dermnet image maybe?
+    func setImage(){
+        
+    }
+    
+    //link to (all or most compatible?)other skin conditions, related by texture, colour, diagnosis method, treatment etc
+    func addLink(){
+        
     }
 }
--- Dermaphone/skinConditionInfoView.swift
+++ Dermaphone/skinConditionInfoView.swift
@@ -5,4 +5,40 @@
 //  Created by Ewan, Aleera C on 02/04/2024.
 //
 
-import Foundation
+import UIKit
+
+class skinConditionInfoView : UIViewController{
+    var name : String? = nil
+    var conditionDescription : String? = nil
+    var conditionCauses : String? = nil
+    var conditionPeople : String? = nil
+    var conditionImage : UIImage? = nil
+    var treatment : String? = nil
+    var diagnosisMethod : String? = nil
+    var features : [String]? = nil
+    var editMode : Bool = false
+    @IBOutlet weak var modelImage: UIImageView!
+    @IBOutlet weak var editButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        editButton.setTitle("Done", for: .highlighted)//does this go here?
+        editButton.setTitle("Edit", for: .normal)
+    }
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        if !editMode {
+            editButton.isHighlighted = true
+            editMode = true
+        }
+        else{
+            editButton.isHighlighted = false
+            editMode = false
+        }
+    }
+    
+    func setImage(image : UIImage){
+        modelImage.image = image
+    }
+}

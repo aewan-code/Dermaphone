079af9682c723f66cb0f7e0d3ca3619e5fa23756
aewan-code <aleera.ewan@outlook.com>
2024-05-26 10:23:06 +0100

    tidied code

--- pythonServer.py
+++ pythonServer.py
@@ -1,93 +1,3 @@
-'''from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
-
-# Define the request handler class
-class MyRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Handle GET requests
-        if self.path == '/upload':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            print("get upload")
-            self.wfile.write(b"Hello, world! This is the response from your server for the /upload path.")
-        else:
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            print("error get")
-            self.wfile.write(b"Hello, world! This is the response from your server.")
-    def do_POST(self):
-        # Handle POST requests (file uploads)
-        if self.path == '/upload':
-            #print("post upload")
-            #content_length = int(self.headers['Content-Length'])
-            
-            #post_data = self.rfile.read(content_length)
-            # Process the uploaded file data
-            # You can save the file or perform any other necessary processing here
-            
-            #self.send_response(200)
-            #self.send_header('Content-type', 'text/html')
-            #self.end_headers()
-            #self.wfile.write(b"File uploaded successfully!") # Confirmation message
-
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            print(post_data)
-            # Split the request data by the boundary between each image
-            boundary = post_data.split(b'\r\n')[0]
-            image_data = post_data.split(boundary)[1:-1]  # Exclude the first and last empty elements
-
-            # Iterate over each image data and save it to the server's file system
-            for image in image_data:
-                # Extract the filename from the content-disposition header
-                filename_start = image.find(b'filename="') + len(b'filename="')
-                filename_end = image.find(b'"', filename_start)
-                filename = image[filename_start:filename_end].decode()
-
-                # Extract the image data
-                data_start = image.find(b'\r\n\r\n') + len(b'\r\n\r\n')
-                image_data = image[data_start:]
-
-                # Save the image to the server's file system
-               # with open(filename, 'wb') as f:
-                #    f.write(image_data)
-                # Specify the folder path and filename
-                folder_path = '/images'
-                filename = 'your_file_name.jpg'
-
-                # Combine folder path and filename to create the full file path
-                full_file_path = os.path.join(folder_path, filename)
-
-                # Write data to the specified file
-                with open(full_file_path, 'wb') as f:
-                    f.write(image_data)
-
-            # Send a response to the client indicating success
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b"Files uploaded successfully!") # Confirmation message
-            
-        else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b"404 Not Found")
-            print("error post")
-
-# Define the server address and port
-server_address = ('', 8002)
-
-# Create the HTTP server
-httpd = HTTPServer(server_address, MyRequestHandler)
-
-# Start the server
-print('Starting server...')
-httpd.serve_forever()
-'''
-
 import uuid
 from flask import Flask, request, jsonify
 import os
@@ -141,12 +51,11 @@ def run_process(command, cwd, output_folder, type):
 
 def verify_output_files(output_folder, type):
     if type == 1:
-        print("check here")
         expected_files = ['baked_mesh.usda']
         
     else:
         print("type 2")
-        expected_files = ['baked_mesh.usdz']  # List expected filenames or patterns
+        expected_files = ['baked_mesh.usdz'] 
     files = os.listdir(output_folder)
     return all(file in files for file in expected_files)
 
@@ -175,16 +84,11 @@ def upload():
         saved_files.append(filename)
 
     print(output_folder_path)
-    #command = 'cd /Users/ace20/dermaphonePython/HelloPhotogrammetry\ 2024-05-13\ 23-05-06/Products/usr/local/bin && ./HelloPhotogrammetry {upload_folder_path} {output_folder_path}'
-    #command = 'cd /Users/ace20/dermaphonePython/HelloPhotogrammetry 2024-05-13 23-05-06/Products/usr/local/bin && ./HelloPhotogrammetry Users/ace20/Downloads/Skin5 /Users/ace20/dermaphonePython/Dermaphone/test4'
-    # Option 1: With `./`
     output_path_variable = '/Users/ace20/dermaphonePython/Dermaphone/' + output_folder_path 
     input_path_variable = '/Users/ace20/dermaphonePython/Dermaphone/' + upload_folder_path
 
     command = ['./HelloPhotogrammetry', input_path_variable, output_path_variable]
     success, error = run_process(command=command, cwd='/Users/ace20/dermaphonePython/HelloPhotogrammetry 2024-05-13 23-05-06/Products/usr/local/bin', output_folder=output_folder_path, type=1)
-    #subprocess.check_call(command, cwd='/Users/ace20/dermaphonePython/HelloPhotogrammetry 2024-05-13 23-05-06/Products/usr/local/bin')
-  #  return jsonify({'success': f'{senderName} - {len(image_uploads)} images saved.'}), 200
     if not success:
         print("stopped here")
         return jsonify({'error': 'Failed to process images', 'details': error}), 500
@@ -196,8 +100,7 @@ def upload():
         directory = '/Applications/usdpython/'
 
         # Path for the temporary shell script
-        #script_path = '.' + os.path.join(directory, 'temp_script.sh')
-        script_path = './temp_script.command'# + os.path.join(directory, 'temp_script.sh')
+        script_path = './temp_script.command'
 
 
         # Script content that uses usd.command
@@ -224,18 +127,8 @@ def upload():
         usdzconvert {outputfile} -copytextures
         exit
         """
-        print(f"Script path: {script_path}")
-
-       # print(f"Permissions: {oct(os.stat(script_path).st_mode)}")
-
-        directory1 = os.path.dirname(script_path)
         full_path = "/Applications/usdpython/temp_script.command"
-        print(directory1)
         print(full_path)
-        # Ensure directory exists
-        if not os.path.exists(directory1):
-            print("hey")
-            os.makedirs(directory1)
         # Write the script to the file
         with open(full_path, 'w') as script_file:
             print("check here for script file")
@@ -245,39 +138,20 @@ def upload():
         os.chmod(full_path, 0o755)
 
         # Execute the script
-        print("check here")
         command = ['./temp_script.command']
-       # command = ['./USD.Command']
         try:
-           # subprocess.run(['./temp_script.sh'],cwd='/Applications/usdpython/')
             success, error = run_process(command=command,cwd='/Applications/usdpython',output_folder=output_folder_path,type=2)
-           # result = subprocess.run([script_path], cwd=directory, text=True, capture_output=True)
-           # if result.returncode != 0:
             if not success:
                 print("stopped here")
                 return jsonify({'error': 'Failed to convert file', 'details': error}), 500
             if success:
                 print("success")
+                usdzfile = output_folder_path + '/baked_mesh.usdz'
                 return jsonify({'success': 'converted file', 'details': success}), 200
-            #    print("Error running script:", result.stderr)
-            #else:
-             #   print("Script output:", result.stdout)
         except Exception as e:
             print(f"An error occurred when trying to run the script: {e}")
+        return jsonify({'error': 'Failed to convert file', 'details': error}), 500
         
-        #command = ['./usdzconvert', outputfile, '-copytextures']
-        
-        #success, error = run_process([script_path], cwd='/Applications/usdpython', output_folder=output_folder_path, type=2)
-        # Optionally, clean up the script file after execution
-        # os.remove(script_path)
-        usdzfile = output_folder_path + '/baked_mesh.usdz'
-        # Output results
-  #      if result.returncode == 0:
-  #          print("Success:", result.stdout)
-   #         return jsonify({'success': 'successfully converted file'}), 200
-    #    else:
-     #       print("Error:", result.stderr)
-      #      return jsonify({'error': 'Failed to process images', 'details': result.stderr}), 700
 
 
 if __name__ == '__main__':
